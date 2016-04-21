aa2a96c3943417886e4ad7c3a19192a2f040f16b
Michael Zhang <failed.down@gmail.com>
2016-04-21 01:13:19 +0000

    It's a string...

--- server/api/utils.py
+++ server/api/utils.py
@@ -29,7 +29,7 @@ from models import Config
 def is_setup_complete():
 	obj = Config.query.filter_by(key="setup_complete").first()
 	if obj is None: return False
-	return obj.value == True or obj.value == 1
+	return obj.value == True or int(obj.value) == 1
 
 def hash_password(s):
 	return generate_password_hash(s)
