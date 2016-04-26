a9dbcc7ae84a9eea56600014d4a3a2b5f0dc75a6
Michael Zhang <failed.down@gmail.com>
2016-04-26 02:44:37 -0500

    Replace config.py

--- server/config.py
+++ server/config.py
@@ -12,16 +12,16 @@ secret.close()
 
 SECRET_KEY = key
 
-SQLALCHEMY_DATABASE_URI = "mysql://user:pass@host:port/db"
+SQLALCHEMY_DATABASE_URI = "mysql://root:i_hate_passwords@localhost/openctf"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 ROOT_FOLDER = os.path.abspath(os.path.join(os.path.realpath(__file__), '../..'))
 
 UPLOAD_FOLDER = os.path.abspath(os.path.join(ROOT_FOLDER, "web/files"))
 
-MAILGUN_URL = ""
-MAILGUN_KEY = ""
-ADMIN_EMAIL = ""
+MAILGUN_URL = "https://api.mailgun.net/v3/example.com"
+MAILGUN_KEY = "key-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ADMIN_EMAIL = "EasyCTF Team <team@easyctf.com>"
 
 GRADER_FOLDER = os.path.abspath(os.path.join(ROOT_FOLDER, "server/graders"))
 
