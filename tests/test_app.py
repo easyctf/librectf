ad3c2b1972d539088ad21d2bf100965fba4cfba6
James Wang <jameswang9909@hotmail.com>
2016-05-05 10:03:31 -0400

    Use correct environmental variable

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,11 @@
 from flask.ext.testing import TestCase
 
 from server.app import app
+from server.config import options
 
 class AppTestCase(TestCase):
 	def create_app(self):
+		app.config['SQLALCHEMY_DATABASE_URI'] = options.SQLALCHEMY_TEST_DATABASE_URI
 		app.config['TESTING'] = True
 		return app
 
