c1b04a99af7fee06d1debcaca995f70d7db36e0e
James Wang <jameswang9909@hotmail.com>
2016-05-13 22:42:13 -0400

    Use unittest instead of flask-testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,25 +1,23 @@
 from flask.ext.testing import TestCase
+import unittest
+import json
 
 from server.app import app
 from server.config import options
 
-class AppTestCase(TestCase):
-	def create_app(self):
-		app.config['SQLALCHEMY_DATABASE_URI'] = options.SQLALCHEMY_TEST_DATABASE_URI
-		app.config['TESTING'] = True
-		return app
-
+class AppTestCase(unittest.TestCase):
 	def setUp(self):
-		pass
+		app.config["TESTING"] = True
 
 	def tearDown(self):
 		pass
 
 	def test_api_is_up_and_running(self):
-		response = self.client.get("/api").json
-		response = dict([(str(k), v) for k, v in response.items()])
-		self.assertEquals(response, dict(success=1, message="The API is online."))
+		with app.test_client() as client:
+			response = json.loads(client.get("/api").data)
+			self.assertEquals(response, dict(success=1, message="The API is online."))
 
 	def test_404(self):
-		response = self.client.get("/four-oh-four")
-		self.assert404(response)
+		with app.test_client() as client:
+			response = client.get("/four-oh-four").status_code
+			self.assertEquals(response, 404)
