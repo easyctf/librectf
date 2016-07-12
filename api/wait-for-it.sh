817ef3bba69f1c17ce6263ddc6804f01601f991e
Michael Zhang <me@michaelz.xyz>
2016-07-11 22:17:49 -0500

    MIGRATE

--- api/wait-for-it.sh
+++ api/wait-for-it.sh
@@ -6,10 +6,10 @@ host=$1
 shift
 cmd="$@"
 
-until mysql -h db -u root -p"password"; do
+until mysql -h db -u root -p"$MYSQL_ROOT_PASSWORD"; do
   >&2 echo "mysql is unavailable - sleeping"
   sleep 1
 done
 
 >&2 echo "mysql is up - executing command"
-exec gunicorn --bind 0.0.0.0:8000 -w 4 app:app
+exec bash -c 'python manage.py db upgrade && gunicorn --bind 0.0.0.0:8000 -w 4 app:app'
