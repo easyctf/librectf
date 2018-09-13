829c20a24b6d4a3144e55e55b6d2af557770cd7c
Michael Zhang <failed.down@gmail.com>
2018-09-13 02:41:34 -0500

    add to the database migration

--- migrations/2018-09-13-071954_initial/down.sql
+++ migrations/2018-09-13-071954_initial/down.sql
@@ -1,1 +1,4 @@
+DROP TABLE `challenges`;
+DROP TABLE `config`;
 DROP TABLE `users`;
+DROP TABLE `teams`;
--- migrations/2018-09-13-071954_initial/up.sql
+++ migrations/2018-09-13-071954_initial/up.sql
@@ -1,3 +1,40 @@
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INTEGER PRIMARY KEY
+CREATE TABLE IF NOT EXISTS `challenges` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(64) NOT NULL,
+    `description` TEXT NOT NULL,
+    `hint` TEXT,
+    `value` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
 );
+CREATE INDEX challenges_value_idx ON `challenges`(`value`);
+
+CREATE TABLE IF NOT EXISTS `config` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `teams` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `teamname` VARCHAR(32) NOT NULL UNIQUE,
+    `affiliation` VARCHAR(32),
+    `banned` BOOLEAN NOT NULL DEFAULT FALSE,
+
+    PRIMARY KEY (`id`)
+);
+CREATE INDEX teams_teamname_idx ON `teams`(`teamname`);
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `tid` INTEGER,
+    `admin` BOOLEAN NOT NULL DEFAULT FALSE,
+    `email` VARCHAR(128) NOT NULL UNIQUE,
+    `password` VARCHAR(128) NOT NULL UNIQUE,
+
+    `date_created` DATETIME NOT NULL DEFAULT NOW(),
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`tid`) REFERENCES `teams`(`id`)
+);
+CREATE INDEX users_email_idx ON `users`(`email`);
