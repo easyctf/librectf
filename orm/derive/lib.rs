aa9b68220a1b9a03e39fff9c0b0141e100fd07ca
Michael Zhang <failed.down@gmail.com>
2018-09-14 04:03:20 -0500

    build schema

--- orm/derive/lib.rs
+++ orm/derive/lib.rs
@@ -1,22 +1,42 @@
 // TODO: merge into codegen
 
 extern crate proc_macro;
+extern crate proc_macro2;
 extern crate syn;
 
 #[macro_use]
 extern crate quote;
 
 use proc_macro::TokenStream;
-use syn::{Item, ItemStruct};
+use proc_macro2::TokenStream as TokenStream2;
+use syn::{Item, ItemMod};
 
 #[proc_macro_attribute]
 pub fn schema_attr(_attrs: TokenStream, item: TokenStream) -> TokenStream {
-    // println!("SCHEMA_ATTR attrs: {:?}", attrs);
-    // println!("SCHEMA_ATTR item: {:?}", item);
+    let item: ItemMod = syn::parse(item).expect("#[schema_attr] is expecting a mod");
+    let vis = &item.vis;
+    let ident = &item.ident;
 
-    let item: Item = syn::parse(item).unwrap();
+    // all of the contents should be #[model]s or #[table]s
+    let mut content = TokenStream2::new();
+    if let Some((_, ref items)) = item.content {
+        for item in items {
+            let item = match item {
+                Item::Struct(struct_item) => struct_item,
+                _ => panic!("only structs should appear in schema!{}")
+            };
+            let vis = &item.vis;
+            let ident = &item.ident;
+            content.extend(quote! {
+                #vis struct #ident {}
+            });
+        }
+    }
+
     let result = quote! {
-        #item
+        #vis mod #ident {
+            #content
+        }
     };
     result.into()
 }
