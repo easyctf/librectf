170e5ee609ede9788dc369492ca101c6ea47db88
Michael Zhang <failed.down@gmail.com>
2018-11-24 14:23:07 -0600

    run migrations

--- components/admin/src/lib.rs
+++ components/admin/src/lib.rs
@@ -1,20 +1,25 @@
 extern crate core;
 extern crate diesel;
+#[macro_use]
+extern crate embed;
 extern crate hyper;
 #[macro_use]
 extern crate log;
+extern crate migrations_internals;
 extern crate multipart;
 #[macro_use]
 extern crate failure;
 extern crate serde;
-extern crate toml;
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
 extern crate structopt;
+extern crate tempfile;
+extern crate toml;
 
 mod chal;
 mod config;
+mod migrate;
 mod util;
 
 use failure::Error;
@@ -23,16 +28,25 @@ use structopt::StructOpt;
 use chal::ImportChalCommand;
 use config::Config;
 pub use config::ConfigWrapper;
+use migrate::MigrateCommand;
+
+#[derive(Embed)]
+#[folder = "migrations"]
+struct Migrations;
 
 #[derive(StructOpt)]
 pub enum AdminCommand {
     /// Import challenges from a directory.
     #[structopt(name = "import")]
     Import(ImportChalCommand),
+
+    #[structopt(name = "migrate")]
+    Migrate(MigrateCommand),
 }
 
 pub fn run(cmd: &AdminCommand, config: &Config) -> Result<(), Error> {
     match cmd {
         AdminCommand::Import(cmd) => cmd.run(config),
+        AdminCommand::Migrate(cmd) => cmd.run(config),
     }
 }
---
+++ components/admin/src/migrate.rs
@@ -0,0 +1,53 @@
+use std::fs::{create_dir_all, File};
+use std::io::{self, Write};
+
+use failure::Error;
+use migrations_internals::{
+    mark_migrations_in_directory, run_migrations, search_for_migrations_directory,
+};
+use tempfile::TempDir;
+
+use util::establish_connection;
+use Config;
+use Migrations;
+
+#[derive(StructOpt)]
+pub struct MigrateCommand {}
+
+impl MigrateCommand {
+    pub fn run(&self, config: &Config) -> Result<(), Error> {
+        let dir = TempDir::new()?;
+
+        // hack: write all the files and then run migrations
+        // TODO: figure out how to construct migrations
+
+        let migrations_dir = dir.path().join("migrations");
+        for filename in Migrations::list() {
+            let path = migrations_dir.join(filename);
+            if let Some(parent) = path.parent() {
+                create_dir_all(parent)?;
+            }
+
+            let mut file = File::create(&path)?;
+            file.write_all(&Migrations::get(filename).unwrap())?;
+            println!("{:?}", path);
+        }
+
+        let conn = establish_connection(config)
+            .expect("Couldn't connect to database. Did you specify DATABASE_URL?");
+
+        // run the migrations
+        let migrations_dir = search_for_migrations_directory(dir.path())?;
+        let migrations = mark_migrations_in_directory(&conn, &migrations_dir)?;
+
+        for (migration, _) in migrations {
+            run_migrations(
+                &conn,
+                vec![migration],
+                &mut io::stdout(),
+            )?;
+        }
+
+        Ok(())
+    }
+}
